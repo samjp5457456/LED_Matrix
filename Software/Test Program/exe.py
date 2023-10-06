e0d7c5550048fa8374a3221d0a71dd34c32b3693
Alex Rodriguez <99780537+roddy-54@users.noreply.github.com>
2023-10-06 10:31:29 -0600

    Update exe.py

--- Software/Test Program/exe.py
+++ Software/Test Program/exe.py
@@ -1,13 +1,20 @@
 import tkinter
+
+rows = 16
+columns = 16
+
+btns = [[None for i in range(rows)] for j in range(columns)]
 master = tkinter.Tk()
 master.title("My First Application!")
 master.geometry("750x750")
 
-for x in range(1, 17):
-    for y in range(1, 17):
-        b = tkinter.Button(master, text="button", command=master.destroy,
-                           height=2, width=2, bg="black",fg="white")
-        b.pack()
-        b.grid(row=x, column=y)
+def changeColor(btn):
+    btn.configure(bg="blue")
+
+for i in range(rows):
+    for j in range(columns):
+        btns[i][j]=Button(master, padx=10, bg="white")
+        btns[i][j].grid(row = i, column = j)
+        btns[i][j]['command'] = lambda btn=btns[i][j]:changeColor(btn)
 
 master.mainloop()
