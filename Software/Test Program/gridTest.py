259a55eee06363eee4604e98fb135906ed121f14
Alex Rodriguez <99780537+roddy-54@users.noreply.github.com>
2023-10-11 16:55:55 -0600

    Progress! Yayyy

--- Software/Test Program/gridTest.py
+++ Software/Test Program/gridTest.py
@@ -1,38 +1,53 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 def createGrid():
     for i in range(rows):
         for j in range(columns):
-            btns[i][j] = Button(tk, padx=10, bg='white')
+            btns[i][j] = Button(tk, padx=10, bg="gray")
             btns[i][j]['command'] = lambda btn=btns[i][j]: color(btn)
             btns[i][j].grid(row=i, column=j)
 
 
 def createColorButtons():
     for i in range(len(colors)):
-        colorButtons[i] = Button(tk, text=colors[i], fg='white', bg=colors[i].lower())
+        if (colors[i] == "White" or colors[i] == "Yellow"):    
+            colorButtons[i] = Button(tk, text=colors[i], fg='black', bg=colors[i].lower())
+        else:
+            colorButtons[i] = Button(tk, text=colors[i], fg='white', bg=colors[i].lower())
+            
         colorButtons[i]['command'] = lambda btn=colorButtons[i]: changeColor(btn)
         colorButtons[i].grid(row=i+5, column=20)
 
 
 def color(btn):
-    btn.configure(bg=userColor)
+    if (userColor == ""):
+        print("No color selected")
+    else:
+        btn['bg'] = userColor
 
 
 def changeColor(btn):
-    userColor = btn['text'].lower()
+    global userColor
+    userColor = btn['bg']
+    
+def on_closing():
+    userColor = "gray"
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        tk.destroy()
 
 
 rows = 16
 columns = 16
 btns = [[None for i in range(rows)] for j in range(columns)]
-colors = ["White", "Red", "Green", "Blue", "Black"]
+colors = ["White", "Red", "Green", "Blue", "Black", "Cyan", "Yellow"]
 colorButtons = [None for i in range(len(colors))]
 tk = Tk()
 tk.title("Pixel Board")
-userColor = "SystemButtonFace"
 
 createGrid()
 createColorButtons()
+
+tk.protocol("WM_DELETE_WINDOW", on_closing)
 tk.mainloop()
